e41ed99ed7b482fd3dbff16048aa90a79f46c187
Michael Veselov <michael.veselov@gmail.com>
2023-02-28 16:50:46 +0300

    update path to assets

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -2,7 +2,7 @@ const Success = (props) => {
   const { count } = props;
   return (
     <div className='success-block'>
-      <img src='public/assets/success.svg' alt='Success' />
+      <img src='public/success.svg' alt='Success' />
       <h3>Done!</h3>
       <p>An invitation has been sent to {count > 1 ? `${count} users` : `${count} user`}.</p>
       <a href='/'>
